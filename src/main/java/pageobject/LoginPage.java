40982dcbfc88088c6ad294e21b821473e60b21d8
PriyankaVarude <priyanka.varude@cuelogic.com>
2020-07-07 15:08:16 +0530

    Updated Xpath

--- src/main/java/pageobject/LoginPage.java
+++ src/main/java/pageobject/LoginPage.java
@@ -14,12 +14,10 @@ import Base.BaseActions;
 public class LoginPage extends BaseActions
 {
 
-	//WebDriver  driver;
-	
 	By home_login = By.xpath("//button[text()='Login']");
 	By email = By.id("tooltip_auth_login_email");
 	By password = By.id("tooltip_auth_login_password");
-	By submit = By.xpath("//button[@class='a-button f-b f-b-293B50-a f-b-293B50-h f-b-4A90E2 f-c-FFFFFF f-h-40 f-l-40 f-m-a f-p-10-h f-r-20 f-s-14 f-w-175 f-600 pure-input-1'][text()='Log In']");
+	By submit = By.xpath("//button[text()='Log In']");
 	By errorMsg=By.xpath("//div[@class='f-m-a f-w-280-m p-validate-error']");
 		
 
